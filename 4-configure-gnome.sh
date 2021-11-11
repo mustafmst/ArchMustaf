a0fb0f07e305fa73ab9c624d02f57ed4195f3610
Paweł Mstowski <pawel.mstowskio@gmail.com>
2021-11-11 17:12:29 +0100

    add gnome extensions and wallpaper

--- 4-configure-gnome.sh
+++ 4-configure-gnome.sh
@@ -29,6 +29,11 @@ PKGS=(
 'gnome-system-monitor',
 'mutter'
 'sushi'
+# extensions
+'gnome-shell-extension-gtile'
+'gnome-menus'
+'gnome-shell-extensions'
+'gnome-shell-extension-pop-shell'
 )
 
 for PKG in "${PKGS[@]}"; do
@@ -47,3 +52,29 @@ systemctl start gdm.service
 systemctl enable gdm.service
 
 # ------------------------------------------------------------------------
+echo -e "\nInstall arch and AUR missing extensions"
+
+# dash to dock
+wget https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v71.shell-extension.zip
+unzip dash-to-dock@micxgx.gmail.com.zip \
+-d /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/
+
+
+echo -e "\nEnabling GNOME shell extensions"
+
+gnome-extensions enable gravatar@jr.rlabs.io
+gnome-extensions enable gsconnect@andyholmes.github.io
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable sound-output-device-chooser@kgshank.net
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+gnome-extensions enable drive-menu@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable pamac-updates@manjaro.org
+gnome-extensions enable places-menu@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable pop-shell@system76.com
+gnome-extensions enable workspace-indicator@gnome-shell-extensions.gcampax.github.com
+
+echo -e "\nCopy and set background image"
+
+mv ~/ArchMustaf/backgrounds/* /usr/share/backgrounds
+gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/arch-1.jpg
+# gsettings set 
