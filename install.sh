9acd3b7c077d535a86f232e8a9884c86c897a0a7
Paweł Mstowski <pawel.mstowskio@gmail.com>
2021-11-11 13:27:08 +0100

    improve gnome installation and add locale

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@
     source /mnt/root/ArchMustaf/install.conf
     arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchMustaf/2-user.sh
     arch-chroot /mnt /root/ArchMustaf/3-post-setup.sh
+    arch-chroot /mnt /root/ArchMustaf/4-configure-gnome.sh
