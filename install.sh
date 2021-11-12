f95542fc03c84eafd010b20830cbb970f6f66c2f
Paweł Mstowski <pawel.mstowskio@gmail.com>
2021-11-12 18:15:35 +0100

    add gnome configuration script

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ arch-chroot /mnt /root/ArchMustaf/1-setup-de.sh
 arch-chroot /mnt /root/ArchMustaf/2-setup-pkg.sh
 arch-chroot /mnt /root/ArchMustaf/3-create-user.sh
 arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchMustaf/4-user.sh
-arch-chroot /mnt /root/ArchMustaf/5-postinstall.sh
+arch-chroot /mnt /root/ArchMustaf/5-configure-gnome.sh
+arch-chroot /mnt /root/ArchMustaf/6-postinstall.sh
 exit
