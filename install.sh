21a4a9885f1d7c5eca31a7971449c703cd05d4db
Paweł Mstowski <pawel.mstowskio@gmail.com>
2021-11-12 16:13:12 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 bash 0-preinstall.sh
-arch-chroot /mnt 
-cd /root/ArchMustaf
-source install.conf
-./1-setup-de.sh
-./2-setup-pkg.sh
-./3-user.sh
-./4-postinstall.sh
+arch-chroot /mnt /root/ArchMustaf/1-setup-de.sh
+arch-chroot /mnt /root/ArchMustaf/2-setup-pkg.sh
+/usr/bin/runuser -u $username -- /home/$username/ArchMustaf/3-user.sh
+arch-chroot /mnt /root/ArchMustaf/4-postinstall.sh
 exit
