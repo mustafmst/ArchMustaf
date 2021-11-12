f85d1c10d1174d8bd20f2c682018284380469b1e
Paweł Mstowski <pawel.mstowskio@gmail.com>
2021-11-12 17:26:04 +0100

    user creation fixes

--- 3-create-user.sh
+++ 3-create-user.sh
@@ -15,16 +15,19 @@
 echo -e "\nDone!\n"
 if ! source ${HOME}/ArchMustaf/install.conf; then
 	read -p "Please enter username:" username
-echo "username=$username" >> ${HOME}/ArchMustaf/install.conf
+    read -p "Please name your machine:" nameofmachine
+    echo "username=$username" >> ${HOME}/ArchMustaf/install.conf
 fi
+
+echo -e "username=$username | nameofmachine=$nameofmachine"
 if [ $(whoami) = "root"  ];
 then
     useradd -m -G wheel,libvirt -s /bin/bash $username 
 	passwd $username
 	cp -R /root/ArchMustaf /home/$username/
     chown -R $username: /home/$username/ArchMustaf
-	read -p "Please name your machine:" nameofmachine
 	echo $nameofmachine > /etc/hostname
 else
 	echo "You are already a user proceed with aur installs"
 fi
+read -p "Press any key to continue..." _
